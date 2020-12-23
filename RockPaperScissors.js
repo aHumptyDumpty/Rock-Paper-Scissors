7b6bc580d7822d3a6a9b4e5d347406fdff03b7f1
aHumptyDumpty <bryanarriaga.arriaga@gmail.com>
2020-12-23 15:27:49 -0600

    added bootstrap + jQuery to project

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,48 +1,59 @@
-function computerPlay(){
+//created "selectionArray" to hold selection of strings
+let selectionArray = ["Rock", "Paper", "Scissors"];
+var result = "";
 
-    //created "selectionArray" to hold selection of strings 
-    let selectionArray = ["Rock", "Paper", "Scissors"];
-
-    //set selection to grab a random string from selectionArray at random 
-    let compSelection = selectionArray[Math.floor(Math.random() * selectionArray.length)]
-
-    //returns selected random string from array to console
-    return compSelection;
+//Grab random value from selectionArray and passed to humanSelection
+function humanChoice() {
+  let humanSelection =
+    selectionArray[Math.floor(Math.random() * selectionArray.length)];
+  return humanSelection;
 }
 
-// function startGame() that takes in two parameters 
-function startGame(playerSelection, computerSelection){
-    //initialized variable result to empty string
-    let result = "";
-
-    // if- if/else statements to match player and computer selections and store winner
-    if(computerSelection == "Scissors" && playerSelection == "Rock"){
-        result = "Human Wins!";
-    }
-    else if(computerSelection == "Paper" && playerSelection == "Rock"){
-        result = "Robot Wins";
-    }
-    else if(computerSelection == "Rock" && playerSelection == "Rock"){
-        result = "Its a Draw!"
-    }
-
-    //return winner of game to variable result
-    return result;
+//Grab random value from selectionArraay and passed to compSelection
+function computerPlay() {
+  let compSelection =
+    selectionArray[Math.floor(Math.random() * selectionArray.length)];
+  return compSelection;
 }
 
-// constants created for playerSelection & computerSelection
-const playerSelection = "Rock";
+const playerSelection = humanChoice();
 const computerSelection = computerPlay();
 
-//created function playFiveTimes to play 5 games of RPS
-function playFiveTimes(){
-    for(let i = 0; i < 5; i++){
-        startGame();
-    }
+
+
+
+// function startGame() that takes in two parameters
+function startGame(computerSelection, playerSelection) {
+  // if- if/else statements to match player and computer selections and store winner
+  if (computerSelection === "Scissors" && playerSelection === "Rock") {
+    result = "Human Wins!";
+  } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+    result = "Robot Wins!";
+  } else if (computerSelection === "Rock" && playerSelection === "Rock") {
+    result = "Its a Draw!";
+  } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+    result = "Robot Wins!";
+  } else if (computerSelection === "Rock" && playerSelection === "Paper") {
+    result = "Human Wins!";
+  } else if (computerSelection === "Paper" && playerSelection === "Paper") {
+    result = "It's a Draw!";
+  } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+    result = "Robot Wins!";
+  } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+    result = "Human Wins!";
+  } else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
+    result = "It's a Draw!";
+  }
+  return result;
 }
- 
 
-// print out 1 game of rock paper scissors to console
-//console.log(startGame(playerSelection, computerSelection))
+let winner = startGame(computerSelection, playerSelection);
 
-console.log(playFiveTimes());          
+
+
+$("#player-img").attr("src", "Images/" + playerSelection + ".png");
+$("#computer-img").attr("src", "Images/" + computerSelection + ".png");
+$("#human-choice").text(playerSelection + " Was Chosen");
+$("#robot-choice").text(computerSelection + " Was Chosen");
+
+$("#display-winner").text(result);
